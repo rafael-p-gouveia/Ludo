5b26df92a755d7b71eeb8c9ecc9e058a871c15ca
Rafael Araujo Tetzner <rafaelaraujotetzner@gmail.com>
2022-09-06 15:00:11 -0300

    Add RGBY pieces images and loaded them

--- ludo/const.py
+++ ludo/const.py
@@ -1,8 +1,24 @@
 
-WIN_DIM  = WIDTH, HEIGHT = (512, 512)
+WIN_DIM  = WIDTH, HEIGHT = (510, 510)
 WIN_NAME = "L U D O !"
 FPS      = 60
 
 BOARD_POS = (0, 0)
 BOARD_DIM = WIN_DIM
-BOARD_IMG = "ludo/img/board.png"
+BOARD_PTH = "ludo/img/board.png"
+
+NUM_TILES = 15
+TILE_DIM  = BOARD_DIM[0] / NUM_TILES, BOARD_DIM[1] / NUM_TILES
+
+BLUE_PIECE   = 1
+RED_PIECE    = 2
+YELLOW_PIECE = 4
+GREEN_PIECE  = 8
+
+BLUE_PTH    = "ludo/img/blue_p.png"
+RED_PTH     = "ludo/img/red_p.png"
+YELLOW_PTH  = "ludo/img/yellow_p.png"
+GREEN_PTH   = "ludo/img/green_p.png"
+
+
+
--- ludo/screen.py
+++ ludo/screen.py
@@ -1,20 +1,29 @@
 import imp
 import pygame
-from ludo.const import BOARD_DIM, BOARD_IMG, BOARD_POS, FPS, WIN_DIM, WIN_NAME
+from ludo.const import *
 import os
 
 class Screen:
+    def update(self):
+        self._clk.tick(FPS)
+        self._window.blit(self._board, BOARD_POS)
+        pygame.display.flip()
+
+    def _load_img(self, path, dim):
+        img = pygame.image.load(os.path.join(path)).convert()
+        img = pygame.transform.smoothscale(img, dim)
+        return img
+
+
     def __init__(self):
         self._window = pygame.display.set_mode(WIN_DIM)
-        self._board_img = pygame.image.load(os.path.join(BOARD_IMG)).convert()
-        self._board_img = pygame.transform.smoothscale(self._board_img, BOARD_DIM)
+        self._board = self._load_img(BOARD_PTH, BOARD_DIM)
+        self._green = self._load_img(GREEN_PTH, TILE_DIM)
+        self._yellow = self._load_img(YELLOW_PTH, TILE_DIM)
+        self._red = self._load_img(RED_PTH, TILE_DIM)
+        self._blue = self._load_img(BLUE_PTH, TILE_DIM)
         self._clk = pygame.time.Clock()
         pygame.display.set_caption(WIN_NAME)
     
-    def update(self):
-        self._clk.tick(FPS)
-        self._window.blit(self._board_img, BOARD_POS)
-        pygame.display.flip()
-
-
+        
 
